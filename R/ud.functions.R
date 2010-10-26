46b9073fd22eb2cb6c6f1c45c4fc8811595e69fd
James Hiebert <hiebert@uvic.ca>
2010-10-26 14:34:24 -0700

    Modified ud.convert to be able to handle NA values

--- R/ud.functions.R
+++ R/ud.functions.R
@@ -16,17 +16,22 @@ function(x, u1, u2) {
   if (! ud.are.convertible(u1, u2)) {
     stop(paste("Units", u1, "and", u2, "are not convertible"))
   }
-  len <- length(x)
-  rv <- .C('R_ut_convert',
-           as.double(x),
+  ## Filter out NA's before passing them to the C function
+  ## since it can't handle them
+  rv <- rep(NA, length(x))
+  i <- which(! is.na(x))
+
+  len <- length(i)
+  c.rv <- .C('R_ut_convert',
+           as.double(x[i]),
            as.integer(len),
            as.character(u1),
            as.character(u2),
            converted=double(len),
            PACKAGE='udunits2'
            )
-  rv <- rv$converted
-  # If it's a matrix/vector or anything else, convert it back to it's original type
+  rv[i] <- c.rv$converted
+  ## If it's a matrix/vector or anything else, convert it back to it's original type
   attributes(rv) <- attributes(x)
   return(rv)
 }
